4baed1a384f63076cc92282570a1ddb934b91f23
Amon Mugiraneza <amon.mugiraneza@samsclub.com>
2018-12-25 13:17:18 -0600

    Made a text view

--- app/src/main/java/com/example/amonmugiraneza/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/example/amonmugiraneza/kotlinproject/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.amonmugiraneza.kotlinproject
 
-import android.support.v7.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.amonmugiraneza.kotlinproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
